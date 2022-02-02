566ffacd110063b36a3c49f170028a9fdc58d46e
nobalpha <roni.kolukisayan@nds.k12.tr>
2022-02-03 01:31:10 +0300

    [fix] variable naming and object structure

--- app/Controllers/Http/CharactersController.ts
+++ app/Controllers/Http/CharactersController.ts
@@ -1,39 +1,39 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Character from 'App/Models/Character'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Character from "App/Models/Character";
 
 export default class CharactersController {
+  public dataName = "characters";
 
-  public dataName='characters';
-  
-  public async index ({view}: HttpContextContract) {
-    const datas=await Character.all();
-    const columnsDefinitions=Character.$columnsDefinitions;
-    return view.render("defaultViews/index",{columnsDefinitions,datas,dataName:this.dataName});
+  public async index({ view }: HttpContextContract) {
+    const characters = await Character.all();
+    const columnsDefinitions = Character.$columnsDefinitions;
+    return view.render("defaultViews/index", {
+      columnsDefinitions: Character.$columnsDefinitions,
+      data: characters,
+      dataName: this.dataName,
+    });
   }
 
-  public async create ({view}: HttpContextContract) {
-    const creationType=await Character.$computedDefinitions;
-    return view.render('defaultViews/create',{creationType,dataName:this.dataName})
+  public async create({ view }: HttpContextContract) {
+    const creationType = await Character.$computedDefinitions;
+    return view.render("defaultViews/create", {
+      creationType,
+      dataName: this.dataName,
+    });
   }
 
-  public async store ({}: HttpContextContract) {
-    
-  }
+  public async store({}: HttpContextContract) {}
 
-  public async show ({view,params}: HttpContextContract) {
+  public async show({ view, params }: HttpContextContract) {
     const data = await Character.findOrFail(params.id);
-    return view.render("defaultViews/show",{data,dataName:this.dataName});
+    return view.render("defaultViews/show", { data, dataName: this.dataName });
   }
 
-  public async edit ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async update ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async destroy ({params}: HttpContextContract) {
-    await Character.query()
-      .where("id", params.id)
-      .delete();
+  public async destroy({ params }: HttpContextContract) {
+    await Character.query().where("id", params.id).delete();
   }
 }
