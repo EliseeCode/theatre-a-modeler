4e3cb9b00198aaaaaabd127c1acda13911db5346
nobalpha <roni.kolukisayan@nds.k12.tr>
2022-02-04 00:20:34 +0300

    [fix] camelcase update on scene model

--- database/seeders/6-Scene.ts
+++ database/seeders/6-Scene.ts
@@ -1,45 +1,45 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import Scene from 'App/Models/Scene';
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import Scene from "App/Models/Scene";
 
 export default class SceneSeeder extends BaseSeeder {
-  public async run () {
+  public async run() {
     // Write your database queries inside the run method
     await Scene.createMany([
       {
-        name: 'Act 1 Scene 1',
+        name: "Act 1 Scene 1",
         position: 0,
-        description:"Une scene d'introduction des personnages",
-        creator_id:1,
-        play_id:1,
+        description: "Une scene d'introduction des personnages",
+        creatorId: 1,
+        playId: 1,
       },
       {
-        name: 'Act 1 Scene 2',
+        name: "Act 1 Scene 2",
         position: 1,
-        description:"Une scene avec des personnages",
-        creator_id:1,
-        play_id:1,
+        description: "Une scene avec des personnages",
+        creatorId: 1,
+        playId: 1,
       },
       {
-        name: 'Act 1 Scene 3',
+        name: "Act 1 Scene 3",
         position: 2,
-        description:"Une troisieme scene avec des personnages",
-        creator_id:1,
-        play_id:1,
+        description: "Une troisieme scene avec des personnages",
+        creatorId: 1,
+        playId: 1,
       },
       {
-        name: 'Intro',
+        name: "Intro",
         position: 0,
-        description:"Ou est Godot?",
-        creator_id:1,
-        play_id:2,
+        description: "Ou est Godot?",
+        creatorId: 1,
+        playId: 2,
       },
       {
-        name: '2e partie',
+        name: "2e partie",
         position: 1,
-        description:"Godot...",
-        creator_id:1,
-        play_id:2,
-      }
+        description: "Godot...",
+        creatorId: 1,
+        playId: 2,
+      },
     ]);
   }
 }
