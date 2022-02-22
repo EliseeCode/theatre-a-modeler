d86547595fd4f1885540765418a6188d28c2adf6
Roni <roni.kolukisayan@nds.k12.tr>
2022-02-23 00:49:21 +0300

    [update] added email & password changers

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,21 +1,57 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
-import Role from 'Contracts/enums/Role';
-
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import Role from "Contracts/enums/Role";
 
 export default class UsersController {
   public async index({ view }: HttpContextContract) {
-
-    const users = await User.all()
+    const users = await User.all();
     return view.render("user.index", {
-      users
+      users,
     });
   }
   public async profile({ auth, view }: HttpContextContract) {
-    return view.render('user.profile', { 'user': auth.user, Role })
+    return view.render("user.profile", { user: auth.user, Role });
   }
 
-  public async RoleUpdateByUser({ auth, request, response }: HttpContextContract) {
+  public async changePassword({
+    request,
+    auth,
+    response,
+  }: HttpContextContract) {
+    const user = await auth.authenticate();
+    const password = request.all().password;
+    if (password) {
+      user.password = password;
+      await user.save();
+      await auth.logout();
+      return response.json({ success: true });
+    }
+    return response.json({ success: false });
+  }
+
+  public async changeMail({ request, auth, response }: HttpContextContract) {
+    const user = await auth.authenticate();
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const email = request.all().email;
+    const assertion = emailRegex.test(email);
+    if (assertion) {
+      console.log(`Mail regex check passed for ${email} ...`);
+      user.email = email;
+      await user.save();
+      return response.json({ email });
+      return response.redirect().back();
+    }
+    return response.internalServerError(
+      "Regex check failed, invalid email form..."
+    );
+  }
+
+  public async RoleUpdateByUser({
+    auth,
+    request,
+    response,
+  }: HttpContextContract) {
     const user = await auth.authenticate();
     const roleId = parseInt(request.all().role);
     if (![Role.STUDENT, Role.TEACHER].includes(roleId)) {
@@ -23,40 +59,43 @@ export default class UsersController {
     }
     user.roleId = roleId;
     await user.save();
-    return response.redirect('back');
+    return response.redirect("back");
   }
 
   public async giveAdminRole({ auth, params, response }: HttpContextContract) {
     const askingUser = await auth.authenticate();
-    if (askingUser.roleId != Role.ADMIN) { return response.redirect('back'); }
+    if (askingUser.roleId != Role.ADMIN) {
+      return response.redirect("back");
+    }
     const id = params.id;
     const user = await User.findOrFail(id);
     user.roleId = Role.ADMIN;
     await user.save();
-    return response.redirect('back');
+    return response.redirect("back");
   }
-  public async removeAdminRole({ auth, params, response }: HttpContextContract) {
+  public async removeAdminRole({
+    auth,
+    params,
+    response,
+  }: HttpContextContract) {
     const askingUser = await auth.authenticate();
-    if (askingUser.roleId != Role.ADMIN) { return response.redirect('back'); }
+    if (askingUser.roleId != Role.ADMIN) {
+      return response.redirect("back");
+    }
     const id = params.id;
     const user = await User.findOrFail(id);
     user.roleId = Role.STUDENT;
     await user.save();
-    return response.redirect('back');
+    return response.redirect("back");
   }
   public async create({ view }: HttpContextContract) {
-
-    return view.render('formation.create')
-
+    return view.render("formation.create");
   }
 
   public async destroy({ params, response }: HttpContextContract) {
-    await User.query()
-      .where("id", params.id)
-      .delete();
+    await User.query().where("id", params.id).delete();
 
-
-    return response.redirect('back')
+    return response.redirect("back");
     //return 404;
   }
 }
