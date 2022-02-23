2c81d5e4bedd2ee26695a7006f90fb8a27c4957d
elisee <reclus.elisee@gmail.com>
2022-02-23 17:31:38 +0300

    initialisation seeder

---
+++ database/seeders/Initialisation.ts
@@ -0,0 +1,20 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User';
+import Version from 'App/Models/Version';
+
+export default class InitialisationSeeder extends BaseSeeder {
+  public async run() {
+    // Write your database queries inside the run method
+    await User.create({
+      loginId: "admin",
+      password: "adminadmin",
+      username: "admin",
+      roleId: 4,
+    }),
+      await Version.create({
+        id: 1,
+        name: "Official",
+        creatorId: 1
+      });
+
+  }
