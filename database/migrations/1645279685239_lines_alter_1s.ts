d7b76eac739d14c49ff7a5ac7840169f6144aaee
elisee <reclus.elisee@gmail.com>
2022-02-25 08:48:51 +0300

    remove Unique Constraint

--- database/migrations/1645279685239_lines_alter_1s.ts
+++ database/migrations/1645279685239_lines_alter_1s.ts
@@ -1,18 +1,23 @@
-import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+// import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
-export default class LinesAlter_1s extends BaseSchema {
-  protected tableName = "lines";
+// export default class LinesAlter_1s extends BaseSchema {
+//   protected tableName = "lines";
 
-  public async up() {
-    this.schema.alterTable(this.tableName, (table) => {
-      table.unique(["position", "scene_id", "character_id", "version_id"]);
-    });
-  }
+//   public async up() {
+//     this.schema.alterTable(this.tableName, (table) => {
+//       table.unique(["position", "scene_id", "character_id", "version_id"]);
+//     });
+//   }
 
-  public async down() {
-    this.schema.alterTable(this.tableName, (table) => {
-      // table.dropIndex(["creator_id", "scene_id", "character_id", "version_id"]);
-      table.dropUnique(["position", "scene_id", "character_id", "version_id"]);
-    });
-  }
-}
+//   public async down() {
+//     this.schema.alterTable(this.tableName, (table) => {
+//       // table.dropIndex(["creator_id", "scene_id", "character_id", "version_id"]);
+//       table.dropUnique([
+//         "position",
+//         "scene_id",
+//         "character_id",
+//         "version_id",
+//       ]);
+//     });
+//   }
+// }
--- database/migrations/1645691382465_lines_alter2s.ts
+++ database/migrations/1645691382465_lines_alter2s.ts
@@ -1,22 +1,22 @@
-import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+// import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
-export default class LinesAlter_1s extends BaseSchema {
-  protected tableName = "lines";
+// export default class LinesAlter_1s extends BaseSchema {
+//   protected tableName = "lines";
 
-  public async up() {
-    this.schema.alterTable(this.tableName, (table) => {
-      table.dropUnique([
-        "creator_id", "scene_id", "character_id", "version_id"
-      ]);
+//   public async up() {
+//     this.schema.alterTable(this.tableName, (table) => {
+//       table.dropUnique([
+//         "creator_id", "scene_id", "character_id", "version_id"
+//       ]);
 
 
-    });
-  }
+//     });
+//   }
 
-  public async down() {
-    this.schema.alterTable(this.tableName, (table) => {
-      // table.dropIndex(["creator_id", "scene_id", "character_id", "version_id"]);
-      table.unique(["creator_id", "scene_id", "character_id", "version_id"]);
-    });
-  }
-}
+//   public async down() {
+//     this.schema.alterTable(this.tableName, (table) => {
+//       // table.dropIndex(["creator_id", "scene_id", "character_id", "version_id"]);
+//       table.unique(["creator_id", "scene_id", "character_id", "version_id"]);
+//     });
+//   }
+// }
